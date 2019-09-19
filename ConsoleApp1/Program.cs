6f47918b1be74ef988357710ed6798c938675ca1
unknown <lequangquochung1912@gmail.com>
2019-09-20 00:37:28 +0700

    m

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            create();
+           
+        }
+        static void create()
+        {
+            int n = int.Parse(Console.ReadLine());
+            ArrayList arr = new ArrayList(n);
+            Console.WriteLine("nhap vao");
+            for (int i = 0; i < arr.Capacity; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                arr.Add(num);
+            }
+
+            for (int i = 0; i < arr.Capacity; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("Insert");
+            Insert(n, arr);
+            Console.WriteLine("remove");
+            removeArrList(n, arr);
+
+        }
+        
+
+        static void Insert( int index, ArrayList arr) 
+        {
+            Console.WriteLine("Nhap Index");
+            index = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap Value");
+            int val = int.Parse(Console.ReadLine());
+            arr.Insert(index, val);
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        static void removeArrList(int index, ArrayList arr)
+        {
+            Console.WriteLine("Nhap Index");
+            index = int.Parse(Console.ReadLine());
+            arr.Remove(index);
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+
+
+        
+
+
+        //private static int IndexOutOfRangeException(ArrayList arr, int index)
+        //{ 
+        //    index = int.Parse(Console.ReadLine());            
+        //    for ( int i = 0; i < arr.Capacity; i++)
+        //    {
+        //        arr[i] = arr[i + 1];
+        //    }
+        //    return ArrayList[arr];
+
+        //}
+    }
+}
